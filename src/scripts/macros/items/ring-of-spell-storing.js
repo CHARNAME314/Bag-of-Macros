57a73ac82f0ea8213ad0330c1aa24920fd445416
Andy Haygood <ahaygood86@gmail.com>
2024-01-18 23:30:00 -0600

    working on add spells component

--- src/scripts/macros/items/ring-of-spell-storing.js
+++ src/scripts/macros/items/ring-of-spell-storing.js
@@ -7,7 +7,50 @@
 ////spell will temporarily be copied over into caster's spellbook (probably as an innate spell) and then cast and then deleted
 ////make sure deletion of spell item happens when item is cast (or if casting fails).  Make sure the inventory on the ring isn't changed until the spell is actually cast.
 //note to self: do the second part first as it will be more challenging I'm sure
+//also, need to add a button that clears the spells on the ring and resets it 
 
+//TODO:
+//Add template macro call
+
+//where I left off: working on adding spells
+
+import {getDialogueButtonType} from "../../helper-functions.js"
+//refactor how you're going to store strings
+import {ringOfSpellStoringInitChoices, ringOfSpellStoringInitHeader} from "../../strings.js"
+
+const addSpells = async (tokenActor, item) => {
+	const actors = await getLiveActors()
+	const eligibleSpells = await getEligibleSpells(actors)
+	console.log("eligibleSpells")
+	console.log(eligibleSpells)	
+	//prompt a dialog to pick who you want to get the spell from (buttons)
+	//this choice will prompt another dialog (radio?  select?) for you to choose your spell from
+	//this choice will add a flag to the ring with the appropriate data unless the ring is already too full (will prompt message)
+}
+const castSpell = async (tokenActor) => {
+	const spellData = await getSpellToCast()
+	const [tempItem] = await createTempItem(spellData, tokenActor)
+	console.log("tempItem")
+	console.log(tempItem)	
+	Hooks.once("dnd5e.preUseItem", (item, config) => {
+		console.log("PRE USE ITEM")
+		if (item.uuid != tempItem.uuid) return false
+		console.log("useItem item")
+		console.log(item)
+		console.log("useItem config")
+		console.log(config)	
+		config.consumeResource = false
+		config.consumeSpellSlot = false
+		config.consumeUsage = false
+		//update to spellLevel when done testing
+		config.slotLevel = 9
+	})	
+	const workflow = await MidiQOL.completeItemUse(tempItem)
+	//don't forget to call the template macro here!
+	console.log("workflow")
+	console.log(workflow)
+	if (workflow) await deleteTempItem(workflow)	
+}
 const createTempItem = async (spellData, tokenActor) => {
 	const sourceItem = await fromUuid(spellData.uuid)
 	console.log("tokenActor")
@@ -28,63 +71,128 @@ const createTempItem = async (spellData, tokenActor) => {
 	console.log(itemData)	
 	return await tokenActor.createEmbeddedDocuments("Item", [itemData])
 }
+const deleteTempItem = async (workflow) => {
+	const template = await fromUuid(workflow.templateUuid) ?? {}
+	console.log("template")
+	console.log(template)		
+	//remove spell data from ring
+	//will add this in after I finish the 'add to ring' part
+	
+	if (template.flags.templatemacro.whenCreated) {
+		Hooks.once("deleteMeasuredTemplate", (deletedTemplate) => {
+			if (deletedTemplate.uuid == template.uuid) {
+				const tempItemExists = fromUuidSync(tempItem.uuid)
+				if (tempItemExists) tempItem.delete()
+			}
+		})
+	} else {
+		tempItem.delete()
+	}	
+}
+const getAttackBonus = (actor, item, ability) => {
+	const isRangedAttack = item.system.actionType == "rsak"
+	const isMeleeAttack = item.system.actionType == "msak"
+	if (!isRangedAttack || !isMeleeAttack) {
+		return 0
+	} else if (isRangedAttack) {
+		return actor.system.abilities[ability].mod + actor.system.bonuses.rsak.attack
+	} else if (isMeleeAttack) {
+		return actor.system.abilities[ability].mod + actor.system.bonuses.msak.attack
+	}
+}
+const getEligibleSpells = async (actors) => {
+	return actors.map(actor => {
+		const highestSpellLevel = getHighestSpellLevel(actor.system.spells)
+		const eligibleItems = actor.items.filter(item => item.type == "spell" && item.system.level > 0 && item.system.level <= 5)	
+		const actorSpells = eligibleItems.reduce((items, item) => {
+			let arr = []
+			let spellData = {}
+			for (let i = item.system.level; i <= 5; i ++) {
+				if (item.system.preparation.mode == "atwill" || item.system.preparation.mode == "innate") {				
+					if (i == item.system.level) {
+						spellData = getSpellData(actor, item, item.system.level)
+						arr.push(spellData)
+					}
+				} else if (item.system.preparation.mode == "pact") {
+					if (i == actor.system.spells.pact.level) {
+						spellData = getSpellData(actor, item, actor.system.spells.pact.level)
+						arr.push(spellData)
+					}
+				} else {
+					if (i <= highestSpellLevel) {
+						spellData = getSpellData(actor, item, i)
+						arr.push(spellData)					
+					}
+				}	
+			}		
+			return [...items, ...arr]
+		}, [])
+		return {origin: actor.prototypeToken.name, spells: actorSpells}
+	})
+}
+const getHighestSpellLevel = (spells) => {
+	let arr = []
+	let label
+	for (let i = 1; i <= 9; i++) {
+		label = "spell" + i
+		if (spells[label].max > 0) arr.push(i)
+	}
+	return Math.max(...arr)
+}
+const getInitIconPaths = (buttonName) => {
+	switch (buttonName) {
+		case "Add Spells":
+			return "icons/magic/defensive/shield-barrier-flaming-diamond-teal-purple.webp"
+			break
+		case "Cast Spell":
+			return "icons/magic/defensive/shield-barrier-flaming-pentagon-teal-purple.webp"
+			break
+		case "Empty Spells":
+			return "icons/magic/defensive/shield-barrier-glowing-triangle-teal-purple.webp"
+			break			
+	}
+}
+const getLiveActors = async () => {
+	return game.users.filter(user => user.character).filter(user => {
+		return canvas.scene.tokens.find(token => token.actor.uuid == user.character.uuid)
+	}).map(user => user.character)
+}
+const getSpellData = (actor, item, i) => {
+	const origin = actor.prototypeToken.name
+	const name = item.name
+	const level = i
+	const dc = actor.system.attributes.spelldc
+	const ability = actor.system.attributes.spellcasting
+	const attackBonus = getAttackBonus(actor, item, ability)
+	const prof = actor.system.attributes.prof	
+	const icon = actor.prototypeToken.texture.src
+	return {origin, name, level, dc, ability, attackBonus, prof, icon}
+}
 const getSpellToCast = async () => {
 	//dialog to chose spell from what's stored on the ring
 	//dummy data for now
 	return {name: "Cure Wounds", uuid: "Item.jDSEZWO8xBbW8AXB", level: 1}
 }
-const main = async ({args}) => {
+const main = async ({args, item}) => {
 	//dialog to choose if you are adding a spell or using a spell
-	//if adding, do add logic
 	console.log(args)
-	//if using, do use logic
+	const initChoice = await getDialogueButtonType(ringOfSpellStoringInitChoices, {width: 400, height: 150}, ringOfSpellStoringInitHeader, getInitIconPaths, 60, 60)
 	const tokenActor = (await fromUuid(args[0].tokenUuid)).actor
+	const liveItem = await fromUuid(item.uuid)
+	console.log("initChoice")
+	console.log(initChoice)	
 	console.log("tokenActor")
-	console.log(tokenActor)
-	const spellData = await getSpellToCast()
-	const [tempItem] = await createTempItem(spellData, tokenActor)
-	console.log("tempItem")
-	console.log(tempItem)	
-	Hooks.once("dnd5e.preUseItem", (item, config) => {
-		console.log("PRE USE ITEM")
-		if (item.uuid != tempItem.uuid) return false
-		console.log("useItem item")
-		console.log(item)
-		console.log("useItem config")
-		console.log(config)	
-		config.consumeResource = false
-		config.consumeSpellSlot = false
-		config.consumeUsage = false
-		//update to spellLevel when done testing
-		config.slotLevel = 9
-	})	
-	const workflow = await MidiQOL.completeItemUse(tempItem)
-	console.log("workflow")
-	console.log(workflow)	
-	const template = await fromUuid(workflow.templateUuid) ?? {}
-	console.log("template")
-	console.log(template)		
-	//tempItem.use()
-	//remove spell data from ring
-	//gotta delete item once template is destroyed, not immediately.  Effects seem fine.  Probably a hook would be best here.
-	//tempItem.delete()
-	
-	if (template.flags.templatemacro.whenCreated) {
-		console.log("delete hook whenCreated")
-		//console.log(butt)		
-		Hooks.once("deleteMeasuredTemplate", (deletedTemplate) => {
-			if (deletedTemplate.uuid == template.uuid) {
-				const butt = fromUuidSync(tempItem.uuid)
-				console.log("delete hook butt")
-				console.log(butt)
-				if (butt) tempItem.delete()
-			}
-		})
-		//socket.executeAsGM("setMeasuredTemplateDelete", template.uuid)}
-	} else {
-		tempItem.delete()
+	console.log(tokenActor)	
+	if (initChoice.value == ringOfSpellStoringInitChoices[0]) {
+		addSpells(tokenActor, liveItem)
+	} else if (initChoice.value == ringOfSpellStoringInitChoices[1]) {
+		castSpell(tokenActor)
+	} else if (initChoice.value == ringOfSpellStoringInitChoices[2]) {
+		//need logic for reset spell
 	}
+	
 }
+
 
 export const ringOfSpellStoring = {
 	"main": main
---
+++ src/scripts/strings.js
@@ -0,0 +1,2 @@
+export const ringOfSpellStoringInitChoices = ["Add Spells", "Cast Spell", "Empty Spells"]
+export const ringOfSpellStoringInitHeader = "What do you want to cast?"
